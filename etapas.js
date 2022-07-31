f3dbdeab5c4d002b3bb44cbb39a82c4b7adea97a
Shirlene Teles <108750190+shiteles@users.noreply.github.com>
2022-07-30 22:09:08 -0300

    commit init

---
+++ etapas.js
@@ -0,0 +1,50 @@
+let etapas = [
+    {
+        titulo: 'DEPUTADO ESTADUAL',
+        numeros: 5,
+        candidatos: [
+        {
+            numero: '38111',
+            nome: 'Frankie Stein',
+            partido:'Team Monster',
+            fotos:[
+            {url:'38111.jpg', legenda:'Deputado Estadual', small: false}    
+            ]
+        } ,
+        {
+            numero: '77222',
+            nome: 'Draculaura',
+            partido:'Team Vampire',
+            fotos:[
+            {url:'77222.jpg', legenda:'Deputado Estadual'}    
+            ]
+        },   
+        ]
+    },
+    {
+        titulo:'PRESIDENTE',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '99',
+                nome: 'Cleo de Nile',
+                partido:'Team Mummy',
+                vice:'Lagoona Blue',
+                fotos:[
+                {url:'99.jpg', legenda:'Presidente'},
+                {url:'99_2.jpg', legenda:'Vice-Presidente', small: true}    
+                ]
+            },
+            {
+                numero:'84',
+                nome:'Clawdeen Wolf',
+                partido:'Team Wolf',
+                vice:'Ghoulia Yelps',
+                fotos:[
+                {url:'84.jpg', legenda: 'Presidente'},
+                {url:'84_2.jpg', legenda: 'Vice-Presidente', small: true}    
+                ]
+            },
+        ]
+    }
+];
